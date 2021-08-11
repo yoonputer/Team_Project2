9bb7314a528de24d03f2b9c7fdb6c8735aea7ac8
yoonputer <yoonputer@naver.com>
2021-08-11 16:01:54 +0900

    A tag를 이용한 웹페이지간 이동추가 + 불필요한 내용제거

--- forkspoon/forkspoon/urls.py
+++ forkspoon/forkspoon/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('/', include('home.urls')),
     path('member/', include('member.urls')),
     path('trend/', include('trend.urls')),
     path('write/', include('write.urls')),
