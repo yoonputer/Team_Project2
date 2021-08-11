8cab68707ac8771d95749277e4237ddc526738e2
doka <posmane5@gmail.com>
2021-08-11 17:00:56 +0900

    update db

--- forkspoon/write/models.py
+++ forkspoon/write/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 class Essay(models.Model):
     score = models.IntegerField()
-    essay = models.CharField(max_length=1000)
+    essayA = models.CharField(max_length=1000)
+    essayQ = models.CharField(max_length=1000)
+    name = models.CharField(max_length=10)
     
     def __str__(self):
-        return self.name
+        return self.score
+
+
