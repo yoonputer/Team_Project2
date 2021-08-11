d16e485677183749bb7655ac151b98af0db86445
doka <posmane5@gmail.com>
2021-08-11 16:39:05 +0900

    add forkspoon/wirte/database table

--- forkspoon/write/models.py
+++ forkspoon/write/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Essay(models.Model):
+    score = models.IntegerField()
+    essay = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return self.name
