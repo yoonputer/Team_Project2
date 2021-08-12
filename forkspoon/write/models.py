9321e3190d889a6f6d9155b652dfa1d03413e486
doka <posmane5@gmail.com>
2021-08-12 11:08:12 +0900

    change field type char to text

--- forkspoon/write/models.py
+++ forkspoon/write/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Essay(models.Model):
     score = models.IntegerField()
-    essayA = models.CharField(max_length=1000)
-    essayQ = models.CharField(max_length=1000)
-    name = models.CharField(max_length=10)
-    
+    essayA = models.TextField()
+    essayQ = models.TextField()
+    name = models.CharField(max_length=20)
+
     def __str__(self):
         return self.score
-
-
