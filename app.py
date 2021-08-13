6e9a6803307f297899195e6b0c6860427489f75d
doka <posmane5@gmail.com>
2021-08-13 15:21:13 +0900

    fixbug

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
+from multiprocessing.dummy import Pool as ThreadPool
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+
+
+def parming(myurl):
+    try:
+        mylist = []
+        뉴데이터 = requests.get(myurl)
+        soup = BeautifulSoup(뉴데이터.content, "html.parser")
+
+        제목 = soup.select_one('div.viewTitWrap > h2.hd > strong').text
+        점수 = soup.select_one('.grade').text
+        질문 = soup.select('dl.qnaLists > dt >button > span.tx')
+        답변 = soup.select('dl.qnaLists > dd > div.tx')
+
+        for idx, val in enumerate(질문):
+            if '선택한' in val.text:
+                mylist = [제목.strip(), 점수.strip(), val.text, 답변[idx].text]
+                break
+        return mylist
+    except AttributeError as e:
+        print(myurl+" = 별점없음")
+
 
 url = 'https://www.jobkorea.co.kr'
 urllist = []
@@ -9,7 +31,7 @@ urllist = []
 #     'https://www.jobkorea.co.kr/starter/passassay?schTxt=%EC%84%A0%ED%83%9D%ED%95%9C&Page=1')
 
 # 잡코리아의 자소서 페이지의 주소 추출하기
-for i in range(1, 2):
+for i in range(1, 10):
     makingurl = f'https://www.jobkorea.co.kr/starter/passassay?schTxt=%EC%84%A0%ED%83%9D%ED%95%9C&Page={i}'
     데이터 = requests.get(makingurl)
 
@@ -20,27 +42,27 @@ for i in range(1, 2):
 
 print(urllist)  # 주석처리 하셔도됨
 
+
 # 추출한 주소를 토대로 자소서 질답 추출하기
-질문리스트 = []
-for i in range(len(urllist)):
-    뉴데이터 = requests.get(urllist[i])
-    soup = BeautifulSoup(뉴데이터.content, "html.parser")
 
-    제목 = soup.select_one('div.viewTitWrap > h2.hd > strong').text
-    점수 = soup.select_one('.grade').text
-    질문 = soup.select('dl.qnaLists > dt >button > span.tx')
-    답변 = soup.select('dl.qnaLists > dd > div.tx')
 
-    for idx, val in enumerate(질문):
-        if '선택한' in val.text:
-            질문리스트.append([제목.strip(), 점수.strip(), val.text, 답변[idx].text])
-            break
+pool = ThreadPool(16)
+result = pool.map(parming, urllist)
+pool.close()
+pool.join()
+
+print(result)
+result = list(filter(None, result))
+
 
 # 리턴되는 질문리스트는 리스트 타입이고 [ [질문, 답변] ,[질문, 답변] ,[질문, 답변] ,...] 형태로 '선택한'이 들어간 질문만 찾아서 해당 답변을 리턴합니다. 데이터프레임으로 옮겨서 엑셀저장 만 하시면 됩니다. , 한글 그대로 쓰셔도 됩니다.
 # print(질문리스트)  # 주석처리 하셔도됨
 # 데이터프레임으로 저장
-a = pd.DataFrame(질문리스트, columns=['제목', '점수', '질문', '답변'])
+try:
+    a = pd.DataFrame(result, columns=['제목', '점수', '질문', '답변'])
+except TypeError as e:
+    print('pass')
+
 print(a)
 # 엑셀로 저장 하려면 터미널에서 pip install openpyxl 해주세요
-a.to_excel("projectdata.xlsx", index=False)
-
+a.to_excel("projectata.xlsx", index=False)
