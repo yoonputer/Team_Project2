c79a01345081961e17bc02094f1eb234e4e2269c
yoonputer <yoonputer@naver.com>
2021-08-16 10:21:37 +0900

    lotto to DB

--- forkspoon/write/models.py
+++ forkspoon/write/models.py
@@ -17,3 +17,18 @@ class choice(models.Model):
     점수 = models.IntegerField()
     질문 = models.TextField()
     답변 = models.TextField()
+
+class lotto_data(models.Model):
+    a = models.DecimalField(max_digits=20,decimal_places=20)
+    b = models.DecimalField(max_digits=20,decimal_places=20)
+    c = models.DecimalField(max_digits=20,decimal_places=20)
+    d = models.DecimalField(max_digits=20,decimal_places=20)
+    e = models.DecimalField(max_digits=20,decimal_places=20)
+    f = models.DecimalField(max_digits=20,decimal_places=20)
+    g = models.DecimalField(max_digits=20,decimal_places=20)
+    h = models.DecimalField(max_digits=20,decimal_places=20)
+    i = models.DecimalField(max_digits=20,decimal_places=20)
+    j = models.DecimalField(max_digits=20,decimal_places=20)
+    k = models.DecimalField(max_digits=20,decimal_places=20)
+    l = models.DecimalField(max_digits=20,decimal_places=20)
+    m = models.DecimalField(max_digits=20,decimal_places=20)
