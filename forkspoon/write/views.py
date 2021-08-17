feec97088574bb241499a2bdc6d521a7011d0ab1
doka <posmane5@gmail.com>
2021-08-17 09:36:10 +0900

    템플릿수정

--- forkspoon/write/views.py
+++ forkspoon/write/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def writeview(request):
     return render(request, 'write/write.html')
+
+
+def result(request):
+    return render(request, 'write/result.html')
