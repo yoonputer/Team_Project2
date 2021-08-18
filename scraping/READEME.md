3b13dcb4c302ef152cd156d5cda5df495b4b60f9
juni <alicelikesbab@gmail.com>
2021-08-18 21:51:05 +0900

    news_all_data in db

--- scraping/READEME.md
+++ scraping/READEME.md
@@ -1,1 +1,6 @@
-여기는 스크레핑입니다만?
+각 폴더에 대한 설명
+
+choice_scraping
+deeplearning_data
+motive_scraping
+news_scraping
---
+++ scraping/news_scraping/saving_df_pkl.py
@@ -0,0 +1,12 @@
+import sqlite3
+import pickle
+import pandas as pd
+
+data_pkl= pickle.load(open('df.pkl', 'rb'))
+
+
+data_df= pd.DataFrame(data_pkl)
+print(data_df)
+
+connect = sqlite3.connect('../../forkspoon/db.sqlite3')
+data_df.to_sql('news_all_data',connect, if_exists='replace')
