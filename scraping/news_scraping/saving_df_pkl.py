741d808aaf096f85a719a05d6cb55ea26c7dc695
juni <alicelikesbab@gmail.com>
2021-08-19 08:39:08 +0900

    load_sql_news_all_data

--- scraping/news_scraping/saving_df_pkl.py
+++ scraping/news_scraping/saving_df_pkl.py
@@ -3,10 +3,10 @@ import pickle
 import pandas as pd
 
 data_pkl= pickle.load(open('df.pkl', 'rb'))
-
+# print(data_pkl)
 
 data_df= pd.DataFrame(data_pkl)
-print(data_df)
+# print(data_df)
 
 connect = sqlite3.connect('../../forkspoon/db.sqlite3')
 data_df.to_sql('news_all_data',connect, if_exists='replace')
