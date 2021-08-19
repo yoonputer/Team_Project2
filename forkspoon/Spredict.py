db30bb0787759284de3af033803e0197c2c946a6
doka <posmane5@gmail.com>
2021-08-19 10:55:38 +0900

    그래프 추가

--- forkspoon/Spredict.py
+++ forkspoon/Spredict.py
@@ -1,6 +1,7 @@
 # to_web.py
 # -*- coding: utf-8 -*-
-# from eunjeon import Mecab
+import platform
+from eunjeon import Mecab
 from konlpy.tag import Hannanum
 import tensorflow as tf
 import sqlite3
@@ -13,7 +14,8 @@ class Spredict:
     def __init__(self):
         self.connect = sqlite3.connect('db.sqlite3')
         print('1')
-        self.stopwords = pd.read_sql_query('select * from stopwords', self.connect)
+        self.stopwords = pd.read_sql_query(
+            'select * from stopwords', self.connect)
         print('2')
 
         self.ko_stopwords_list = np.array(self.stopwords['words'].tolist())
@@ -22,16 +24,20 @@ class Spredict:
         self.new_model = tf.keras.models.load_model('write/test_dummy_LSTM.h5')
         print('4')
 
-
         with open('write/tokenizers.pkl', 'rb') as f:
             self.tokenizer = pickle.load(f)
 
     def sentance_predict(self, sentence):
+        if 'Windows' in platform.platform():
+            mecab = Mecab()
+        else:
         mecab = Hannanum()
         input_sentance = mecab.morphs(sentence)
-        input_sentance = [tok for tok in input_sentance if tok not in self.ko_stopwords_list]
+        input_sentance = [
+            tok for tok in input_sentance if tok not in self.ko_stopwords_list]
         encoded = self.tokenizer.texts_to_sequences([input_sentance])
-        pad_new = tf.keras.preprocessing.sequence.pad_sequences(encoded, maxlen=300)
+        pad_new = tf.keras.preprocessing.sequence.pad_sequences(
+            encoded, maxlen=300)
         score = self.new_model.predict(pad_new)
         return [score, np.argmax(score)]
 
