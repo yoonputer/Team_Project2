3334c288b817ba67d953d6b081f854575c06a9b7
juni <alicelikesbab@gmail.com>
2021-08-19 11:30:15 +0900

    기업

--- scraping/news_scraping/saving_df_pkl.py
+++ scraping/news_scraping/saving_df_pkl.py
@@ -2,11 +2,11 @@ import sqlite3
 import pickle
 import pandas as pd
 
-data_pkl= pickle.load(open('df.pkl', 'rb'))
+data_pkl= pickle.load(open('groups.pkl', 'rb'))
 # print(data_pkl)
 
 data_df= pd.DataFrame(data_pkl)
 # print(data_df)
 
 connect = sqlite3.connect('../../forkspoon/db.sqlite3')
-data_df.to_sql('news_all_data',connect, if_exists='replace')
+data_df.to_sql('groups',connect, if_exists='replace')
